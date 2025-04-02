e78c20f60a32f7317c7d20b2339155c92be27f5c
oussamaton2206 <touhami200622@gmail.com>
2025-04-02 17:27:11 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,229 @@
+let menu = document.getElementById('links');
+let closeBtn = document.getElementById('closeBtn');
+let toggleBtn = document.getElementById('menuBtn');
+let body = document.getElementById("body");
+
+
+
+if (toggleBtn) {
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.add('show-menu');
+    });
+}
+
+if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('show-menu');
+    });
+}
+
+const links = document.querySelectorAll('.link-item')
+
+function linkAction(){
+    const menu = document.getElementById('links')
+    menu.classList.remove('show-menu')
+}
+links.forEach(link => link.addEventListener('click', linkAction))
+
+
+//pagination pages
+
+let pizzaItems = document.getElementById("pizzaItems");
+let panzarotiItems = document.getElementById("panzarotiItems");
+let mqilaItems = document.getElementById("mqilaItems");
+let poutinsItems = document.getElementById("poutinsItems");
+let pastichioItems = document.getElementById("pastichioItems");
+let shawarmaItems = document.getElementById("shawarmaItems");
+let tacosItems = document.getElementById("tacosItems");
+let sandwitchesItems = document.getElementById("sandwitchesItems");
+let jusItems = document.getElementById("jusItems");
+let saladesItems = document.getElementById("saladesItems");
+
+
+function pizzaMenu(){
+    pizzaItems.style.display = "block";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function panzarotiMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "block";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function mqilaMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "block";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function poutinMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "block";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function pastichioMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "block";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function shawarmaMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "block";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function tacosMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "block";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function sandwichesMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "block";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "none";
+}
+
+function jusMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "block";
+    saladesItems.style.display = "none";
+}
+
+function saladesMenu(){
+    pizzaItems.style.display = "none";
+    panzarotiItems.style.display = "none";
+    mqilaItems.style.display = "none";
+    poutinsItems.style.display = "none";
+    pastichioItems.style.display = "none";
+    shawarmaItems.style.display = "none";
+    tacosItems.style.display = "none";
+    sandwitchesItems.style.display = "none";
+    jusItems.style.display = "none";
+    saladesItems.style.display = "block";
+}
+
+
+
+// on scroll animation 
+
+let leftObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add("showLeftContent");
+        }
+        else{
+            entry.target.classList.remove("showLeftContent")
+        }
+    })
+})
+
+let showFL = document.querySelectorAll(".showFromLeft");
+showFL.forEach((el) => leftObserver.observe(el))
+
+
+let rightObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add("showRightContent");
+        }
+        else{
+            entry.target.classList.remove("showRightContent")
+        }
+    })
+})
+
+let showFR = document.querySelectorAll(".showFromRight");
+showFR.forEach((el) => rightObserver.observe(el))
+
+
+window.addEventListener("scroll", function(){
+    let scrollOrigin = document.documentElement.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let percentage = (scrollOrigin / scrollHeight) * 100;
+    document.querySelector(".progressBar").style.width = percentage + "%"
+})
+
+
+
+//-----------------
+document.oncontextmenu = () => {
+    return false
+}
+document.onkeydown = e => {
+    if (e.key == "F12"){
+        return false
+    }
+    else if (e.ctrlKey && e.key == "u"){
+        return false
+    }
+}
